fb82b6acc9924549f92f816a260c07b11231a6c5
maharani-2102 <hanimaharanipurnamasari@gmail.com>
2021-06-20 02:23:10 +0700

    kalkulator

---
+++ src/components/Heading.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>{{message}}</h1>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            message : "Kalkulator menggunakan Vue.js"
+        }
+    }
+}
+</script>
+
+<style>
+    h1{
+        color: #2785cc;
+    }
+</style>
